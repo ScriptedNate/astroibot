f8fdb49d3e55d12e54b5fe45965aaa66625c3856
ScriptedNate <35863804+ScriptedNate@users.noreply.github.com>
2019-03-08 15:29:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-let coins = require("./coins.json");
-let xp = require("./xp.json");
-let purple = botconfig.purple;
-let cooldown = new Set();
-let cdseconds = 5;
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -49,66 +44,6 @@ bot.on("message", async message => {
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
-  // if(!coins[message.author.id]){
-  //     coins[message.author.id] = {
-  //       coins: 0
-  //     };
-  //   }
-  // 
-  //   let coinAmt = Math.floor(Math.random() * 15) + 1;
-  //   let baseAmt = Math.floor(Math.random() * 15) + 1;
-  //   console.log(`${coinAmt} ; ${baseAmt}`);
-  // 
-  //   if(coinAmt === baseAmt){
-  //     coins[message.author.id] = {
-  //       coins: coins[message.author.id].coins + coinAmt
-  //     };
-  //   fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-  //     if (err) console.log(err)
-  //   });
-  //   let coinEmbed = new Discord.RichEmbed()
-  //   .setAuthor(message.author.username)
-  //   .setColor("#0000FF")
-  //   .addField("💸", `${coinAmt} coins added!`);
-  // 
-  //   message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
-  //   }
-  // 
-  //   let xpAdd = Math.floor(Math.random() * 7) + 8;
-  //   console.log(xpAdd);
-  // 
-  //   if(!xp[message.author.id]){
-  //     xp[message.author.id] = {
-  //       xp: 0,
-  //       level: 1
-  //     };
-  //   }
-  // 
-  // 
-  //   let curxp = xp[message.author.id].xp;
-  //   let curlvl = xp[message.author.id].level;
-  //   let nxtLvl = xp[message.author.id].level * 300;
-  //   xp[message.author.id].xp =  curxp + xpAdd;
-  //   if(nxtLvl <= xp[message.author.id].xp){
-  //     xp[message.author.id].level = curlvl + 1;
-  //     let lvlup = new Discord.RichEmbed()
-  //     .setTitle("Level Up!")
-  //     .setColor(purple)
-  //     .addField("New Level", curlvl + 1);
-  // 
-  //     message.channel.send(lvlup).then(msg => {msg.delete(5000)});
-  //   }
-  //   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-  //     if(err) console.log(err)
-  //   });
-    // let prefix = prefixes[message.guild.id].prefixes;
-    // if(!message.content.startsWith(prefix)) return;
-    // if(cooldown.has(message.author.id)){
-    //   message.delete();
-    //   return message.reply("You have to wait 5 seconds between commands.")
-    // }
-    // if(!message.member.hasPermission("ADMINISTRATOR")){
-    //   cooldown.add(message.author.id);
 //if(cmd === `${prefix}kick`){
 
 //   //!kick @daeshan askin for it
@@ -228,9 +163,6 @@ bot.on("message", async message => {
 //    return message.channel.send(`Hello, Nice to meet you buddy! ${message.author}`);
 //   }
 
-// setTimeout(() => {
-//    cooldown.delete(message.author.id)
-//  }, cdseconds * 1000)
 
 });
 
